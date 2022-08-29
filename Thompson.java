a1c249216ac93b1321c0956e8ca64958ee703e3b
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-08-28 18:29:29 -0600

    empezando a tratar a Thompson

--- Thompson.java
+++ Thompson.java
@@ -9,7 +9,7 @@ public class Thompson {
         ArrayList<String> expresion = new ArrayList<String>();
 
         // Defininendo una pila para el alfabeto.
-        Stack<String> pila = new Stack<String>();
+        Stack<String> alfabeto = new Stack<String>();
 
         // Definiendo una pila para las operaciones de la expresión.
         Stack<String> operaciones = new Stack<String>();
@@ -24,13 +24,13 @@ public class Thompson {
         Stack<Integer> estados_aceptacion = new Stack<Integer>();
 
         // Definiendo una pila para los estados de la expresión.
-        ArrayList<Integer> estados = new ArrayList<Integer>();
+        Stack<Integer> estados = new Stack<Integer>();
 
         // Instanciando la clase de transiciones.
         Transiciones transicion = new Transiciones();
 
         // Definiendo los estados de las transiciones. Esto es una clase aparte.
-        ArrayList<Transiciones> transiciones = new ArrayList<Transiciones>();
+        // ArrayList<Transiciones> transiciones = new ArrayList<Transiciones>();
 
         for (int i = 0; i < postfix.length(); i++) {
             // Pusheando los caracteres de la expresion regular en el arraylist.
@@ -50,25 +50,54 @@ public class Thompson {
                     operaciones.push(String.valueOf(postfix.charAt(i)));
                     break;
                 default: // Agregando el caracter de la expresión a la pila de la expresión regular.
-                    pila.push(String.valueOf(postfix.charAt(i)));
+                    alfabeto.push(String.valueOf(postfix.charAt(i)));
                     break;
             }
 
         }
 
+        System.out.println("Expresión regular: " + alfabeto);
+
         // Recorriendo la pila de la expresión regular.
-        for (int x = 0; x < pila.size(); x++) {
+        for (int x = 0; x < alfabeto.size(); x++) {
             // System.out.println("Caracter: " + pila.get(x));
             // Pusheando los estados de la expresión regular a un arraylist, iniciando en 0.
-            estados.add(x);
+            estados.push(x);
         }
 
-        System.out.println("Estados: " + estados);
+        // System.out.println("Estados: " + estados);
 
         // System.out.println("Expresion regular dentro de un stack: " + expresion);
-        // System.out.println("Pila: " + pila);
-        // System.out.println("Operaciones: " + operaciones);
-        // System.out.println("Estados: " + estados);
+        System.out.println("Pila: " + alfabeto);
+        System.out.println("Operaciones: " + operaciones);
+        System.out.println("Estados: " + estados);
+
+        // Recorriendo e imprimiendo la pila de estados y la pila del alfabeto de la
+        // expresión regular.
+        for (int i = 0; i < estados.size(); i++) {
+            System.out.println("Estado: " + estados.get(i));
+            System.out.println("Caracter: " + alfabeto.get(i));
+            System.out.println("");
+
+            // Variable que guarda cada estado inicial y final.
+            int estado_inicial1 = estados.get(i);
+
+            // Variable que guarda cada letra.
+            String letra = alfabeto.get(i);
+
+            // Fabricando las transiciones.
+            transicion.Transicion(estado_inicial1, estado_inicial1 + 1, letra.charAt(0));
+
+        }
+
+        // // Llenar el ArrayList de transiciones.
+        // while (pila.size() > 0) {
+        // System.out.println("Simbolo: " + pila.pop());
+        // System.out.println("Estado: " + estados.pop());
+        // System.out.println("");
+        // // System.out.println("Estado de aceptación: " + estados_aceptacion.get(e));
+        // // transicion.Transicion(estados.get(e), estados_aceptacion.get(e), simbolo);
+        // }
 
         return postfix;
     }
--- Transiciones.java
+++ Transiciones.java
@@ -3,10 +3,15 @@ public class Transiciones {
     int de, a; // de es el estado de inicio y a es el estado de fin.
     char simbolo; // simbolo de la transicion.
 
-    public void Transicion(int de, int a, char c) {
+    public String Transicion(int de, int a, char c) {
         this.de = de;
         this.a = a;
         this.simbolo = c;
+
+        System.out.println("de: " + de + " a: " + a + " simbolo: " + c);
+
+        return this.de + "," + this.a + "," + this.simbolo;
+
     } // Constructor de la clase Transicion. Esto servirá para almacenar las
       // transiciones de los estados.
 }
