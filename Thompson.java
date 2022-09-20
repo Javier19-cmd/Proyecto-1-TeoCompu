4064e51f1318f7b0e5604bcd8e0824c33d694e16
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-09-20 12:10:34 -0600

    quitando mètodo que no sirve

--- Thompson.java
+++ Thompson.java
@@ -226,8 +226,6 @@ public class Thompson {
         // System.out.println(transiciones.get(i).toString());
         // }
 
-        setEstadoInicial_y_Final();
-
     }
 
     // Método para hacer la operación default, que es crear un estado para un
@@ -442,27 +440,6 @@ public class Thompson {
     // Haciendo un getter para el estado inicial.
     public Estado getEstadoInicial() {
         return estados_iniciales.peek();
-    }
-
-    // Haciendo un método para settear formalmente el estado inicial y final.
-    public void setEstadoInicial_y_Final() {
-        // Seteando el estado inicial.
-        Estado inicial = estados_iniciales.pop();
-        inicial.setInicio(true);
-        // Pusheado el estado inicial al Stack de estados.
-        estados_iniciales.push(inicial);
-
-        // Seteando el estado final.
-        Estado fin = estados_aceptacion.pop();
-        fin.setFinal(true);
-
-        // Pusheado el estado final al Stack de estados.
-        estados_aceptacion.push(fin);
-
-        if (alfabeto.contains("&")) { // Si el alfabeto contiene el simbolo de lambda.
-            // Eliminando el simbolo de lambda del alfabeto.
-            alfabeto.remove("&");
-        }
     }
 
     // Haciendo un getter de la lista de transiciones.
